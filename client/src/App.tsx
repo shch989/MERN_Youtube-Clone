d80be644d7f86c8136aa38919551855a25621417
shch989 <tankpop11@gmail.com>
2022-12-18 15:19:53 +0900

    Navbar 퍼블리싱

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,9 +8,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Home />} />
         <Route path='/search' element={<Search />} />
         <Route path='/watch/:id' element={<Watch />} />
+        <Route path='/' element={<Home />} />
       </Routes>
     </BrowserRouter>
   )
